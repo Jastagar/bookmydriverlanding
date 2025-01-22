06df7f8fd9a379bff84d9dec8c3dbb3ab2d7ea4c
Jastagar <jastagar1581.be20@chitkara.edu.in>
2025-01-22 11:15:10 +0530

    added SEO optimization

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Hero from "./sections/Hero";
 
 export default function App() {
   return (
-      <div className="font-sans bg-gradient-to-t from-primary-500 via-primary-700 to-primary-900 min-h-screen text-primary-100 flex flex-col items-center">
+    <div className="font-sans bg-gradient-to-t from-primary-500 via-primary-700 to-primary-900 min-h-screen text-primary-100 flex flex-col items-center">
       <Hero />     
       <About />     
       <Contact />     
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -21,13 +21,13 @@ export default function Hero() {
         <p className="text-lg max-w-lg">The easiest way to book a driver for all your transportational needs. Whether you are a client or a driver, we’ve got you covered.</p>
 
         <div className="flex space-x-4 mt-6">
-          <button onClick={downloadClient} className="bg-primary-600 hover:bg-primary-900 text-white font-bold py-2 px-6 rounded-md transition duration-300 transform hover:scale-105">
+            <button onClick={downloadClient} className="bg-primary-600 hover:bg-primary-900 text-white font-bold py-2 px-6 rounded-md transition duration-300 transform hover:scale-105">
             Book a Driver
-          </button>
-          <button onClick={downloadDriver} className="bg-primary-300 hover:bg-primary-700 text-white font-bold py-2 px-6 rounded-md transition duration-300 transform hover:scale-105">
+            </button>
+            <button onClick={downloadDriver} className="bg-primary-300 hover:bg-primary-700 text-white font-bold py-2 px-6 rounded-md transition duration-300 transform hover:scale-105">
             Be A Driver
-          </button>
+            </button>
         </div>
-      </section>
+    </section>
   )
 }
